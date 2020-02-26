2ab9547286238ed5bcd07c2704656af4cac7f812
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2020-02-26 00:52:25 -0500

    build our scene + jetpack should eventually only go up

--- I Spy/Assets/Scripts/Jetpack.cs
+++ I Spy/Assets/Scripts/Jetpack.cs
@@ -14,7 +14,6 @@ public class Jetpack : MonoBehaviour {
     void Update() {
         if (Input.GetButton("Jump")) {
             rb.AddForce(transform.up * thrust);
-            rb.AddForce(transform.forward * thrust / 2);
         }
     }
 }
