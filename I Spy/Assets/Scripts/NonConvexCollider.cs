de6f4521eb1a91fe1de75898c53eee0c01f587a6
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2020-02-27 21:43:29 -0500

    rename ConvexCollider.cs

---
+++ I Spy/Assets/Scripts/NonConvexCollider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(MeshCollider))]
+public class NonConvexCollider : MonoBehaviour
+{
+	Rigidbody master;
+	// Start is called before the first frame update
+	void Awake()
+	{
+		master = transform.parent.GetComponentInParent<Rigidbody>();
+		var rb = GetComponent<Rigidbody>();
+		rb.mass = master.mass;
+		rb.drag = master.drag;
+		rb.angularDrag = master.angularDrag;
+		rb.isKinematic = true;
+		rb.collisionDetectionMode = CollisionDetectionMode.ContinuousSpeculative;
+	}
+}
